96d0d3acf6546103a922891ca0d441e228ba42a9
Avi Schneider <avipedia1986@gmail.com>
2015-06-03 21:04:47 -0400

    Refactor presenter.js

--- js/presenter.js
+++ js/presenter.js
@@ -1,9 +1,24 @@
 var view = 'league';
 
-$(document).ready(function() {
-    v=$.query.get('view');
-    if (v == 'league' || v =='tournaments' || v == 'join') {
+setView = function() {
+    v = $.query.get('view');
+    if (v == 'league' || v == 'tournaments' || v == 'join') {
         view = v;
     }
-    $('#link-'+view).addClass('active');
+};
+
+loadView = function() {
+    $('#link-' + view).addClass('active');
+    $.get('view/' + view + '.html', '', showInMain);
+};
+
+showInMain = function(txt, stat, xhr) {
+    $('main').html(txt);
+};
+
+$(document).ready(function() {
+    setView();
+    loadView();
 });
+
+
---
+++ view/join.html
@@ -0,0 +1,1 @@
+<h1>join page</h1>
